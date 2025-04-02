3bedf8edde0b144a57fc9c35e61f8cd58efdb343
Duane <ydgb0714@gmail.com>
2025-04-02 17:34:46 +0800

    check

--- src/app/[locale]/unit/[slug]/page.tsx
+++ src/app/[locale]/unit/[slug]/page.tsx
@@ -1,47 +1,9 @@
 import { generateSeoData } from "../../_components/functions/generateSeoData";
 import UnitPage from "./components/UnitPage";
-import type { Metadata, ResolvingMetadata } from 'next'
  
 type Props = {
   params: Promise<{ slug: string }>
 } 
-export async function generateMetadata(
-    { params }: Props,
-    ): Promise<Metadata> {
-    // read route params
-    const { slug } = await params
-
-    const slugString = Array.isArray(slug) ? slug[0] : slug || "";
-    const lastString = slugString.split("-").pop() ?? "";
-
-    const code = lastString.replace(/\D/g, ""); 
-    // fetch data
-    //const product = await fetch(`http://localhost:3000/api/external/units?unitid=${code}`).then((res) => res.json())
-
-
-    const data = await fetch('http://localhost:3000/api/external/units?unitid='+code)
-    const posts = await data.json();
-
-    // optionally access and extend (rather than replace) parent metadata
-    //const previousImages = (await parent).openGraph?.images || []
-
-    const propertyData = {
-        bedrooms: posts[0].bedrooms,
-        propertyType: posts[0].category,
-        adType: posts[0].category,
-        name: posts[0].propertyname,
-        community: posts[0].community,
-        emirate: posts[0].city_name,
-        refNo: posts[0].refNo,
-        seoStart: "",
-    };
-        
-    const seoData = generateSeoData(propertyData);
-
-    return {
-        title: seoData.seoTitle,
-    }
-}
 
 export default async function Page({ params }: Props){
     
