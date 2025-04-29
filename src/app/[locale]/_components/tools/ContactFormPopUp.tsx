ada5ac7d00f740ea8c152956954ea5c7d4492578
Duane <ydgb0714@gmail.com>
2025-04-29 19:39:33 +0800

    Modal forms

--- src/app/[locale]/_components/tools/ContactFormPopUp.tsx
+++ src/app/[locale]/_components/tools/ContactFormPopUp.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
-import "./ContactForm.css";
 import { useTranslation } from "react-i18next";
 
 import {
