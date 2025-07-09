2d419b247451a06c9e4ff60217bf165990ccfe4f
Duane <ydgb0714@gmail.com>
2025-07-09 14:32:54 +0800

    hack fix project id

--- src/app/api/external/projects/route.ts
+++ src/app/api/external/projects/route.ts
@@ -5,6 +5,7 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const query = searchParams.get('query')
     const myHeaders = new Headers();
+    let finquery;
     myHeaders.append("accept", "*/*");
     myHeaders.append(
       "apiKey",
@@ -12,7 +13,15 @@ export async function GET(request: NextRequest) {
     );
     myHeaders.append("Content-Type", "application/json");
   
-    const raw = JSON.stringify({"propertyName": query});
+    switch (query) {
+      case 'jacob co beachfront living by ohana':
+        finquery = 'jacob & co. beachfront living by ohana'
+        break;
+      default:
+        finquery = query
+        break;
+    }
+    const raw = JSON.stringify({"propertyName": finquery});
   
     const requestOptions = {
       method: "POST",
