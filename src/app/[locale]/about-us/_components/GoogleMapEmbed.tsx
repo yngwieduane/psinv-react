89ca5d580720978c300cdd989d09fd945677bb09
neemapssree <145227095+neemapssree@users.noreply.github.com>
2025-12-30 15:25:33 +0400

    translation done

--- src/app/[locale]/about-us/_components/GoogleMapEmbed.tsx
+++ src/app/[locale]/about-us/_components/GoogleMapEmbed.tsx
@@ -2,18 +2,18 @@
 
 import { useEffect, useState, useCallback } from "react";
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
-import { ContactLocation } from "@/data/contactLocations";
+import { psiMapLocation } from "@/data/psiMapLocations";
 
 interface GoogleMapEmbedProps {
-  center: {lat: number, lng: number};
-  locations: ContactLocation[];
-  selectedLocation: ContactLocation | null;
+  center: { lat: number, lng: number };
+  locations: psiMapLocation[];
+  selectedLocation: psiMapLocation | null;
   onMapLoad?: (map: google.maps.Map) => void;
   tabKey?: string;
 }
 
 export default function GoogleMapEmbed({ center, locations, selectedLocation, onMapLoad, tabKey }: GoogleMapEmbedProps) {
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   });
 
@@ -24,20 +24,24 @@ export default function GoogleMapEmbed({ center, locations, selectedLocation, on
     onMapLoad?.(mapInstance);
   }, [onMapLoad]);
 
- useEffect(() => {
-  if(map) {
-    if(selectedLocation) {
-      map.panTo({ lat: selectedLocation.latitude, lng: selectedLocation.longitude });
-      map.setZoom(16);
+  useEffect(() => {
+    if (map) {
+      if (selectedLocation) {
+        map.panTo({ lat: selectedLocation.latitude, lng: selectedLocation.longitude });
+        map.setZoom(16);
+      }
+      else {
+        map.panTo(center);
+        map.setZoom(12);
+      }
     }
-    else {
-      map.panTo(center);
-      map.setZoom(12);
-    }
-  }  
- }, [selectedLocation, center, map, tabKey]);
+  }, [selectedLocation, center, map, tabKey]);
 
- if (!isLoaded) return <div>Loading Map...</div>;
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) return <div>Loading Map...</div>;
 
   return (
     <GoogleMap
@@ -47,17 +51,17 @@ export default function GoogleMapEmbed({ center, locations, selectedLocation, on
       onLoad={onLoad}
     >
       {locations.map((loc) => (
-        <Marker 
-        key={loc.id} 
-        position={{lat: loc.latitude, lng:loc.longitude}} title={loc.name}
-        icon={
-          selectedLocation?.id === loc.id
-            ? "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-            : "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
-        }
+        <Marker
+          key={loc.id}
+          position={{ lat: loc.latitude, lng: loc.longitude }} title={loc.name}
+          icon={
+            selectedLocation?.id === loc.id
+              ? "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+              : "https://maps.google.com/mapfiles/ms/icons/red-dot.png"
+          }
         />
       ))}
-      
+
     </GoogleMap>
   );
 }
