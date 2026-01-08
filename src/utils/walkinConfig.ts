d4cf37fa5fe85ad96d25eac3b30efef910740198
Athirasmohan <athiraasm@gmail.com>
2026-01-08 18:37:10 +0400

    updated agent list in walkin form (conrad and jbr)

--- src/utils/walkinConfig.ts
+++ src/utils/walkinConfig.ts
@@ -1,85 +1,44 @@
 import { WalkinFormConfig } from '@/types/walkinFormTypes';
+import { JBR_ALL_AGENTS } from './walkinAgents/jbrAgents';
 
 export const walkinFormConfig: Record<string, WalkinFormConfig> = {
   "conrad-hotel-at-etihad-tower-abu-dhabi": {
     title: "Conrad Hotel at Etihad Tower, Abu Dhabi",
     developerLogoUrl: "/images/walkin-form/etihad-logo.png",
-    referredbyid:3458,
-    sendto: ["wd3@psinv.net"],
-    branch:"AUH",
+    referredbyid: 3458,
+    sendto: ["wd3@psinv.net", "yngwie.g@psinv.net", "callcenter@psinv.net"],
+    branch: "AUH",
     showClientProfession: true,
     showContactInformation: true,
     showHostedBy: true,
     agentsByDate: {
- "2025-08-12": [
-    { id: "7328", name: "Sabina Umarova" },
-    { id: "7253", name: "Maya Ramos Da Silva" },
-    { id: "7043", name: "Omar Khaleel Subhi Basheer" },
-    { id: "6827", name: "Munif Ali Alsoliman" }
-  ],
-  "2025-08-13": [
-    { id: "7410", name: "Gleybe Dantas Maranhao" },
-    { id: "6714", name: "Wael Fathi Mohammed Abu Mariam" },
-    { id: "7348", name: "Nasir Ra'ed Talal Mushatti" },
-    { id: "7367", name: "Mohammad Amjed Subhi AbuSalah" }
-  ],
-  "2025-08-14": [
-    { id: "7352", name: "Liangyu Xu" },
-    { id: "7284", name: "Uzair Sameer Sameer Javed Aziz" },
-    { id: "6592", name: "Arnay Baiboranova" },
-    { id: "6715", name: "Ahmed Fathi Mohammed Abu Mariam" }
-  ],
-  "2025-08-15": [
-    { id: "6639", name: "Yousef Ahmad Alisha" },
-    { id: "6614", name: "Hanna Jiries Hanna Ibrahim" },
-    { id: "6653", name: "Mahmoud Mahmoud Abdelkader Mekawy" },
-    { id: "6986", name: "Bashar Nazmi Hussein Alswalhi" }
-  ],
-  "2025-08-16": [
-    { id: "6970", name: "Esraa Othman Abo Fares" },
-    { id: "7141", name: "Yaser Nezar Badrah" },
-    { id: "7447", name: "Ahmedkad Hussam Baddour" },
-    { id: "7278", name: "Mohammad Anas Yousef" }
-  ],
-  "2025-08-17": [
-    { id: "6987", name: "Shadi Basem Shrafedin Abofakher" },
-    { id: "7085", name: "Abdullah Sherif Mohamed Ibrahim Aly" },
-    { id: "7352", name: "Liangyu Xu" },
-    { id: "7271", name: "Abdel Rahman Jamal Subhi Hajjir" }
-  ],
-  "2025-08-18": [
-    { id: "7036", name: "George Khalil George Abughazala" },
-    { id: "6885", name: "Kinan Hisham Sheikh Eihara" },
-    { id: "6507", name: "Abdalla Ashraf Ali Mohamed Ahmed" },
-    { id: "7368", name: "Rim Aissaoui" }
-  ],
-  "2025-08-19": [
-    { id: "7278", name: "Mohammad Anas Yousef" },
-    { id: "7447", name: "Almekdad Hussam Baddour" },
-    { id: "4621", name: "Oumaima Rouicha" },
-    { id: "4604", name: "Engy Saied Abdelmonem Ali" }
-  ],
-  "2025-08-20": [
-    { id: "7402", name: "Zayed El Idrissi Alami" },
-    { id: "7369", name: "Rashid Wardeh" },
-    { id: "7401", name: "Ahmed Osama Abdulla Elsayed" },
-    { id: "7081", name: "Amer Mousa Hashem Hato" }
-  ],
-  "2025-08-21": [
-    { id: "7343", name: "Abdelrahman Ahmed Mohamed Attia Mandour" },
-    { id: "7167", name: "Mohamed Rouabhi" },
-    { id: "7205", name: "Michael Borsch" },
-    { id: "7231", name: "Abdulrazzaq Omar Mubarak Balain" }
-  ]
-}
+      "2026-01-08": [
+        { id: "7262", name: "Omar Nazih Hamdache" },
+        { id: "7278", name: "Mohammad Anas Yousef" },
+        { id: "6216", name: "Ahmed Khadragy Ibrahim Moustafa Wasel" },
+        { id: "6350", name: "Mohammad Abduljabbar Bakkour" },
+      ],
+      "2026-01-09": [
+        { id: "6330", name: "Tala Moustapha Darwich" },
+        { id: "7238", name: "Tavleen Kaur Sumit Uppal" },
+        { id: "7467", name: "Maher Archid Hadife" },
+        { id: "7331", name: "Ghalia Chater" },
+      ],
+      "2026-01-10": [
+        { id: "9812", name: "Lais Chalithodi" },
+        { id: "9846", name: "Lakshay Dua" },
+        { id: "7100", name: "Mhd Abul Khir Houmam Alkezbari" },
+        { id: "7356", name: "Malek Saleh Ali Salo" },
+      ]
+    }
   },
   "yas-mall-stand": {
     title: "Yas Mall Walk-In Stand",
-   // developerLogoUrl: "/logos/emaar.png",
-    sendto: ["wd3@psinv.net","yngwie.g@psinv.net"],
-    referredbyid:3458,
+    // developerLogoUrl: "/logos/emaar.png",
+    sendto: ["wd3@psinv.net", "callcenter@psidubai.com"],
+    referredbyid: 3458,
     showClientProfession: true,
-    branch:"AUH",
+    branch: "AUH",
     showContactInformation: false,
     showHostedBy: true,
     agentsByDate: {
@@ -89,13 +48,13 @@ export const walkinFormConfig: Record<string, WalkinFormConfig> = {
       ]
     }
   },
-    "galleria-stand-deyaar": {
+  "galleria-stand-deyaar": {
     title: "Galleria Stand Deyaar",
     developerLogoUrl: "/images/walkin-form/deyaar-logo.svg",
-    sendto: ["wd3@psinv.net", "yngwie.g@psinv.net"],
-    referredbyid:3458,
+    sendto: ["wd3@psinv.net", "callcenter@psinv.net"],
+    referredbyid: 3458,
     showClientProfession: true,
-    branch:"AUH",
+    branch: "AUH",
     showContactInformation: true,
     showHostedBy: true,
     agentsByDate: {
@@ -105,28 +64,23 @@ export const walkinFormConfig: Record<string, WalkinFormConfig> = {
       ]
     }
   },
-    "jbr-lead-registration-dubai": {
+  "jbr-lead-registration-dubai": {
     title: "JBR Lead Registration",
-   // developerLogoUrl: "/logos/emaar.png",
-    sendto: ["wd3@psinv.net","yngwie.g@psinv.net"],
+    // developerLogoUrl: "/logos/emaar.png",
+    sendto: ["wd3@psinv.net", "callcenter@psidubai.com"],
     showClientProfession: true,
-    referredbyid:4421,
-    branch:"dubai",
+    referredbyid: 4421,
+    branch: "dubai",
     showContactInformation: true,
     showHostedBy: true,
-    agentsByDate: {
-      "2025-08-05": [
-        { id: "6169", name: "Alexia Degas" },
-        { id: "7168", name: "Scott Mason" }
-      ]
-    }
+    agents: JBR_ALL_AGENTS,
   },
-      "psi-assets-reem": {
+  "psi-assets-reem": {
     title: "PSI Assets - Reem Branch",
     logo: "/images/walkin-form/psi-assets-logo.png",
-    sendto: ["wd3@psinv.net","yngwie.g@psinv.net"],
+    sendto: ["wd3@psinv.net", "callcenter@psiassets.com"],
     showClientProfession: true,
-    branch:"assets",
+    branch: "assets",
     showContactInformation: true,
     showHostedBy: true,
     agentsByDate: {
@@ -134,18 +88,18 @@ export const walkinFormConfig: Record<string, WalkinFormConfig> = {
         { id: "7056", name: "Abdel Rahman Ayed Abdalla Janem" },
         { id: "4794", name: "PSI Asset" }
       ],
-       "2025-08-15": [
+      "2025-08-15": [
         { id: "7056", name: "Abdel Rahman Ayed Abdalla Janem" },
         { id: "4794", name: "PSI Asset" }
       ]
     }
   },
-        "psi-assets-yas": {
+  "psi-assets-yas": {
     title: "PSI Assets - Yas Branch",
     logo: "/images/walkin-form/psi-assets-logo.png",
-    sendto: ["wd3@psinv.net", "yngwie.g@psinv.net"],
+    sendto: ["wd3@psinv.net", "callcenter@psiassets.com"],
     showClientProfession: false,
-    branch:"assets",
+    branch: "assets",
     showContactInformation: true,
     showHostedBy: true,
     agentsByDate: {
@@ -153,7 +107,7 @@ export const walkinFormConfig: Record<string, WalkinFormConfig> = {
         { id: "7056", name: "Abdel Rahman Ayed Abdalla Janem" },
         { id: "4794", name: "PSI Asset" }
       ],
-       "2025-08-15": [
+      "2025-08-15": [
         { id: "7056", name: "Abdel Rahman Ayed Abdalla Janem" },
         { id: "4794", name: "PSI Asset" }
       ]
