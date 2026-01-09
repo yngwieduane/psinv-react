90688a0aee6d31b6d001c96eae771470aa48ab08
Athirasmohan <athiraasm@gmail.com>
2026-01-09 13:26:24 +0400

    menu and footer update for walkin

--- src/app/[locale]/_components/ConditionalNavigation.tsx
+++ src/app/[locale]/_components/ConditionalNavigation.tsx
@@ -7,9 +7,20 @@ export type Page = '/en' | '/ar' | '/ru' | '/du' | '/cn';
 
 export default function ConditionalNavigation() {
   const pathname = usePathname();
-  const isNoNavPage = ['/list-your-property', '/psi-youngsters-program', '/international', '/luxury-project-uae', '/emirati-hub'].some((path) =>
-    pathname.includes(path)
-  );
+
+  const isNoNavPage =
+    pathname.includes('/walk-in/') || // ✅ ALL walk-in pages
+    [
+      '/list-your-property',
+      '/psi-youngsters-program',
+      '/international',
+      '/luxury-project-uae',
+      '/emirati-hub',
+      '/conrad-abu-dhabi',
+      '/jbr-lead-registration-dubai',
+    ].some((path) => pathname.includes(path));
+
   if (isNoNavPage) return null;
+
   return <Navigation currentPage={pathname as Page} />;
 }
