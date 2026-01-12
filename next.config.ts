7f9dbcef65e0c4acb6122fe301eb5053db08fa16
Duane <ydgb0714@gmail.com>
2026-01-12 14:03:35 +0800

    Added redirect for services

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,46 @@ const nextConfig: NextConfig = {
         destination: '/units',
         permanent: true,
       },
+      {
+        source: '/services/Sodic',
+        destination: 'https://services.psinv.net/Sodic/',
+        permanent: true,
+      },
+      {
+        source: '/services/SaadiyatIslandLP',
+        destination: 'https://services.psinv.net/SaadiyatIslandLP/',
+        permanent: true,
+      },
+      {
+        source: '/services/Roadshow_Checklist',
+        destination: 'https://services.psinv.net/Roadshow_Checklist/',
+        permanent: true,
+      },
+      {
+        source: '/services/RequestsPortal',
+        destination: 'https://services.psinv.net/RequestsPortal/',
+        permanent: true,
+      },
+      {
+        source: '/services/RahaBeach',
+        destination: 'https://services.psinv.net/RahaBeach/',
+        permanent: true,
+      },
+      {
+        source: '/services/NewYearGifts',
+        destination: 'https://services.psinv.net/NewYearGifts/',
+        permanent: true,
+      },
+      {
+        source: '/services/bashayer-apartment-alhudayriat',
+        destination: 'https://services.psinv.net/bashayer-apartment-alhudayriat/',
+        permanent: true,
+      },
+      {
+        source: '/services/CorporateMarketing',
+        destination: 'https://services.psinv.net/CorporateMarketing/',
+        permanent: true,
+      },
     ]
   },
   reactStrictMode: true,
