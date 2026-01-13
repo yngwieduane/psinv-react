bb144da005db99252e3da9cb54e133562660ef98
Anoopi George <>
2026-01-13 14:22:33 +0400

    Updated newsletter data and added thumbnails

--- src/data/newsletters.ts
+++ src/data/newsletters.ts
@@ -2,6 +2,18 @@
 
 export const newsletters = [
   {
+    title: 'A New Launch In The Heart Of Cultural District',
+    date: '12 January 2026 16:20 UTC+04:00 ',
+    image: '/images/newsletter/newsletter-row-thumbnail.webp',
+    link: 'https://marketing.psinv.net/-temporary-slug-fac42ae9-dc05-4d75-bb78-fcb2663f8737?hs_preview=WOpsilQz-342347394277&utm_source=hs_email&utm_medium=email&_hsenc=p2ANqtz-_d3vjXYU_CQf6U1xXQxRvt_l5bVvOjjVFh0Ls4dPgcLsFpBz7ALNVtyiLLj7dnElk9lwU9',
+  },
+  {
+    title: 'Be the First to Explore Yas Islands Newest Project',
+    date: '10 January 2026 12:12 UTC+04:00',
+    image: '/images/newsletter/ohana-newletter-thumbnail',
+    link: 'https://marketing.psinv.net/-temporary-slug-e1aec110-5221-4862-a647-b0485d1dcb06?hs_preview=ZbwjJtcm-341242535157&utm_source=hs_email&utm_medium=email&_hsenc=p2ANqtz--1aQN-PPg5uydDlEhJM2Hx7007k_OyjTKID6UVDEwBuNujQ7npNaNKfTCsTeJFklz-1h9B',
+  },
+  {
     title: 'New Launch in Raha Beach - Bab Al Qasr Canal Residences',
     date: '11 July 2025 14:53 UTC+04:00',
     image: '/images/newsletter/newsletter-thumbnail-1.webp',
@@ -51,12 +63,6 @@ export const newsletters = [
   },
    {
     title: 'Sample 6',
-    date: '10 July 2025 08:00 UTC+04:00',
-    image: '/images/newsletter/newsletter-thumbnail-3.webp',
-    link: '#',
-  },
-   {
-    title: 'Sample 7',
     date: '10 July 2025 08:00 UTC+04:00',
     image: '/images/newsletter/newsletter-thumbnail-3.webp',
     link: '#',
