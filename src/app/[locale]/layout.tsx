94402661ed454b019c55d0f16ebf775588dbbf47
Duane <ydgb0714@gmail.com>
2026-01-22 18:18:32 +0800

    changes hreflang

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -54,8 +54,17 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // Build alternates dynamically
   const languageAlternates: Record<string, string> = {};
 
+  const hreflangMap: Record<string, string> = {
+    en: 'en',
+    ar: 'ar',
+    ru: 'ru',
+    cn: 'zh-CN',
+    de: 'de',
+  };
+
   locales.forEach((lang) => {
-    languageAlternates[lang] = `${siteBaseUrl}/${lang}`;
+    const hreflang = hreflangMap[lang] || lang;
+    languageAlternates[hreflang] = `${siteBaseUrl}/${lang}`;
   });
 
   // Add x-default
@@ -139,16 +148,6 @@ export default async function LocaleLayout({
         </UserProvider>
         {/* <GoogleTagManager gtmId="GTM-KDDP2SR" /> */}
 
-        {/* GTM loaded only after interactive */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KDDP2SR');
-          `}
-        </Script>
       </body>
     </html>
   );
