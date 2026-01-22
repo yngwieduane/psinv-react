2292351274bc3d9a40a3b03d17df1c5185ec7a5f
Duane <ydgb0714@gmail.com>
2026-01-22 16:11:00 +0800

    Added tags

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -139,6 +139,16 @@ export default async function LocaleLayout({
         </UserProvider>
         {/* <GoogleTagManager gtmId="GTM-KDDP2SR" /> */}
 
+        {/* GTM loaded only after interactive */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDDP2SR');
+          `}
+        </Script>
       </body>
     </html>
   );
