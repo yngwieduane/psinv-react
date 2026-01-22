05bf67d37bb3be23bdab2ee9def9f473c4e81fd0
Duane <ydgb0714@gmail.com>
2026-01-22 19:15:23 +0800

    loading changed

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Loading(props: any) {
     return (
-        <div className="fixed inset-0 z-[9999] bg-white/80 backdrop-blur-sm flex items-center justify-center">
+        <div className="fixed inset-0 z-[9999] bg-white backdrop-blur-sm flex items-center justify-center">
             <Image
                 alt="Property Shop Investment"
                 src="/assets/images/psi-gif.gif"
