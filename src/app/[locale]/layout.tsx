61079914efc34983e50781fcfe0386d9784cc6af
Duane <ydgb0714@gmail.com>
2026-02-05 16:53:41 +0800

    undo layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -218,22 +218,13 @@ export default async function LocaleLayout({
         {/* GTM loaded only after interactive */}
         <Script id="gtm-script" strategy="lazyOnload">
           {`
-            function loadGTM() {
-              if (window.gtmLoaded) return;
-              window.gtmLoaded = true;
-
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KDDP2SR');
-            }
-
-            ['scroll','mousemove','touchstart','keydown'].forEach(event => {
-              window.addEventListener(event, loadGTM, { once: true });
-            });
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDDP2SR');
           `}
-      </Script>
+        </Script>
 
       </body>
     </html>
