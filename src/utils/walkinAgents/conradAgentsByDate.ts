8ccf92451e9e1d0ab75a9d5b2aa9b9b870532a4a
Athirasmohan <athiraasm@gmail.com>
2026-03-27 10:26:05 +0400

    Conrad updated agent list

--- src/utils/walkinAgents/conradAgentsByDate.ts
+++ src/utils/walkinAgents/conradAgentsByDate.ts
@@ -8,87 +8,11 @@ export type WalkinAgent = {
 export type AgentsByDate = Record<string, WalkinAgent[]>;
 
 export const CONRAD_AGENTS_BY_DATE: AgentsByDate = {
-"2026-03-16": [
-  { "id": "9936", "name": "Andrew Emad Fouad Fahmy" },
-  { "id": "9985", "name": "Ola Nabil Hamdan" },
-  { "id": "9905", "name": "Mohamed Tarek Ali Mohamed Hussien" },
-  { "id": "7043", "name": "Omar Khaleel Subhi Basheer" },
+"2026-03-27": [
+  { "id": "6987", "name": "Shadi Basem Shrafedin Abofakher" },
+  { "id": "7211", "name": "Petronella Awad" },
   { "id": "9952", "name": "Mahmoud H.K. Oudah" },
-  { "id": "7467", "name": "Maher Archid Hadife" }
-],
-  "2026-03-17": [
-    { "id": "9862", "name": "Aleksandra Vojinovic" },
-    { "id": "9919", "name": "Ahmad Hamza Hussein Harb" },
-    { "id": "7225", "name": "Miguel Angel Aguilar Leon" },
-    { "id": "6152", "name": "Bojana Popovic" },
-    { "id": "9757", "name": "Mohammad Rizik Saleh Bani Hani" },
-    { "id": "7427", "name": "Youmna Ashraf Mohamed Ali Metwally" }
-  ],
-
-  "2026-03-18": [
-    { "id": "7232", "name": "Hamza Nedal Daoud Eleiwat" },
-    { "id": "6614", "name": "Hanna Jiries Hanna Ibrahim" },
-    { "id": "9977", "name": "Rima Ayman Derbas Hassan" },
-    { "id": "9926", "name": "Lynne Abdul Hadi" },
-    { "id": "9906", "name": "Nessren Ghassan Kassis" },
-    { "id": "7331", "name": "Ghalia Chater" }
-  ],
-
-  "2026-03-19": [
-    { "id": "9949", "name": "Charles Sebastian Joseph Robson" },
-    { "id": "7441", "name": "Randa Jamil Assi" },
-    { "id": "6998", "name": "Doina Andreea Bita" },
-    { "id": "7356", "name": "Malek Saleh Ali Salo" },
-    { "id": "6762", "name": "Hanan Aied Sweis" },
-    { "id": "7231", "name": "Abdulrazzaq Omar Mubarak Balain" }
-  ],
-
-  "2026-03-20": [
-    { "id": "9896", "name": "Mariam Saleem Muhammad Saleem" },
-    { "id": "6252", "name": "Nabila Assaad Hage Hassan" },
-    { "id": "6267", "name": "Ali Hekmat Fawzi" },
-    { "id": "5024", "name": "Rasha Bashar Awwad Aljamaeen" },
-    { "id": "7349", "name": "Omeed Aref Mohammad Alzghoul" },
-    { "id": "9949", "name": "Charles Sebastian Joseph Robson" }
-  ],
-  "2026-03-21": [
-    { "id": "9780", "name": "Mohan John Tohn Thomas Kunnathukuzhiyil Varkey Thomas" },
-    { "id": "7097", "name": "Aya Sayed Abdelsalam Ahmed" },
-    { "id": "7254", "name": "Anastasiya Aksatskaya" },
-    { "id": "7225", "name": "Miguel Angel Aguilar Leon" },
-    { "id": "7467", "name": "Maher Archid Hadife" },
-    { "id": "9953", "name": "Yazan Saleh Ibrahim Ghawanmeh" }
-  ],
-  "2026-03-22": [
-    { "id": "9951", "name": "Alfadel Hazem Abdelhameed Mohamed Shoeib" },
-    { "id": "", "name": "Salima Moukhlis" },
-    { "id": "4260", "name": "Nada Yasser Abdalla Ibrahim" },
-    { "id": "7193", "name": "Feliphe de Freitas Santos" },
-    { "id": "7253", "name": "Maya Ramos Da Silva" },
-    { "id": "", "name": "Tarek AbouHuwaij" }
-  ],
-  "2026-03-23": [
-    { "id": "9930", "name": "Nermine Mohamed Mamdouh Refaei Abouelela" },
-    { "id": "7262", "name": "Omar Nazih Hamdache" },
-    { "id": "9904", "name": "Fadi Akram Nasr" },
-    { "id": "6987", "name": "Shadi Basem Shrafedin Abofakher" },
-    { "id": "", "name": "Mostafa El Sayed Katarya" },
-    { "id": "7371", "name": "Mohamad Hilmi Mohamad Al-Shami" }
-  ],
-  "2026-03-24": [
-    { "id": "7211", "name": "Petronella Awad" },
-    { "id": "9935", "name": "Bashar Akl Maryam" },
-    { "id": "7205", "name": "Michael Borsch" },
-    { "id": "6805", "name": "Rabih Fahim Shaar" },
-    { "id": "9952", "name": "Mahmoud H.K. Oudah" },
-    { "id": "9953", "name": "Yazan Saleh Ibrahim Ghawanmeh" }
-  ],
-  "2026-03-25": [
-    { "id": "", "name": "Masud Al Nasr" },
-    { "id": "6345", "name": "Diana Bashirova" },
-    { "id": "9952", "name": "Mahmoud H.K. Oudah" },
-    { "id": "7467", "name": "Maher Archid Hadife" },
-    { "id": "9757", "name": "Mohammad Rizik Saleh Bani Hani" },
-    { "id": "7356", "name": "Malek Saleh Ali Salo" }
-  ]
+  { "id": "6575", "name": "Samer Sultan S. Ishtieh" },
+  { "id": "9935", "name": "Bashar Akl Maryam" }
+]
 };
