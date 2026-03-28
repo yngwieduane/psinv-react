5566a499374f63690d6fe29daa85b175f6afb107
Athirasmohan <athiraasm@gmail.com>
2026-03-28 13:23:20 +0400

    updated conrad agents list for upcoming days

--- src/utils/walkinAgents/conradAgentsByDate.ts
+++ src/utils/walkinAgents/conradAgentsByDate.ts
@@ -22,5 +22,28 @@ export const CONRAD_AGENTS_BY_DATE: AgentsByDate = {
   { "id": "7343", "name": "Abdelrahman Ahmed Mohamed Attia Mandour" },
   { "id": "7205", "name": "Michael Borsch" },
   { "id": "6639", "name": "Yousef Ahmad Alisha" }
+],
+"2026-03-29": [
+  { "id": "9932", "name": "Mostafa Mohamed Abdalaziz Morsi Elbaramawy" },
+  { "id": "7350", "name": "Prakash Dhakal" },
+  { "id": "7189", "name": "Majnun Mavlonazarov" },
+  { "id": "6614", "name": "Hanna Jiries Hanna Ibrahim" },
+  { "id": "6791", "name": "Aymen Sioud" }
+],
+"2026-03-30": [
+  { "id": "6998", "name": "Doina Andreea Bita" },
+  { "id": "9757", "name": "Mohammad Rizik Saleh Bani Hani" },
+  { "id": "6987", "name": "Shadi Basem Shrafedin Abofakher" },
+  { "id": "9905", "name": "Mohamed Tarek Ali Mohamed Hussien" },
+  { "id": "7447", "name": "Almekdad Hussam Baddour" },
+  { "id": "7262", "name": "Omar Nazih Hamdache" }
+],
+"2026-03-31": [
+  { "id": "7367", "name": "Mohammad Amjed Subhi AbuSalah" },
+  { "id": "6575", "name": "Samer Sultan S. Ishtieh" },
+  { "id": "7314", "name": "Ahmed Esmael Shalaby Mostafa" },
+  { "id": "7427", "name": "Youmna Ashraf Mohamed Ali Metwally" },
+  { "id": "7142", "name": "Firas Riyad Abu Haibeh" },
+  { "id": "7253", "name": "Maya Ramos Da Silva" }
 ]
 };
