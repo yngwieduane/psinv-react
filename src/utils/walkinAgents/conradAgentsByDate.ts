4808160f27051b04f158be44863d3cddd83f4dfb
Athirasmohan <athiraasm@gmail.com>
2026-03-28 13:06:48 +0400

    updated conrad agent

--- src/utils/walkinAgents/conradAgentsByDate.ts
+++ src/utils/walkinAgents/conradAgentsByDate.ts
@@ -14,5 +14,13 @@ export const CONRAD_AGENTS_BY_DATE: AgentsByDate = {
   { "id": "9952", "name": "Mahmoud H.K. Oudah" },
   { "id": "6575", "name": "Samer Sultan S. Ishtieh" },
   { "id": "9935", "name": "Bashar Akl Maryam" }
+],
+"2026-03-28": [
+  { "id": "6267", "name": "Ali Hekmat Fawzi" },
+  { "id": "5024", "name": "Rasha Bashar Awwad Aljamaeen" },
+  { "id": "7166", "name": "Ali Nabil Ali Abdelrahman Shatta" },
+  { "id": "7343", "name": "Abdelrahman Ahmed Mohamed Attia Mandour" },
+  { "id": "7205", "name": "Michael Borsch" },
+  { "id": "6639", "name": "Yousef Ahmad Alisha" }
 ]
 };
